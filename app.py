37d8cef9445dc8856c2670c91337ecf5b279c278
im-rcr <69986362+im-rcr@users.noreply.github.com>
2021-12-12 03:37:06 +0530

    edit commit

--- app.py
+++ app.py
@@ -30,6 +30,5 @@ def predict():
         
         return render_template('result.html', prediction=my_prediction)
 
-if __name__ == "__main__":
-    from waitress import serve
-    serve(app, host="0.0.0.0", port=8080)
+if __name__ == '__main__':
+	app.run(debug=True)
